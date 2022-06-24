b61155861c8563756bd83e756143a6f87145bb70
xAkashax <kbilan1992@gmail.com>
2022-06-24 20:47:57 +0200

    add function to js, add readme

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import myData from "./data.json" assert {type:'json'};
+import myData from "./data.json"assert {
+  type: 'json'
+};
 
-// pobrać bary
-// pobrać "cash"
 const chartBars = document.querySelectorAll('.bar');
 const total = document.querySelector('.cash');
 
@@ -11,15 +11,86 @@ function summary() {
     return acc + value.amount;
   }, 0);
   total.innerHTML = `$ ${sum}`;
- 
 }
+
+//The current day's bar highlighted in a different colour to the other bars
+function setActiveDay() {
+  let today = new Date();
+  let dayId = today.getDay() - 1; 
+  chartBars[dayId].classList.add("today");
+}
+
+//Dynamically generated bars based on the data provided in the local JSON file
+function calcMaxBarHeight(maxPrice) {
+  return (100 * maxPrice) / 80;
+}
+
+function setHeightOfBars() {
+  const maxValue = calcMaxValue();
+  const maxHeight = calcMaxBarHeight(maxValue);
+
+  chartBars.forEach(bar => {
+    let obj = myData.find(o => o.day === bar.id);
+    let height = Math.round((obj.amount * 100) / maxHeight);
+    bar.style.height = `${height}%`;
+  });
+}
+
+function calcMaxValue() {
+  let maxValue = myData[0].amount;
+
+  for (let i = 1; i < myData.length; i++) {
+    if (myData[i].amount > maxValue) {
+      maxValue = myData[i].amount;
+    }
+  }
+  return maxValue;
+}
+
+//function mousover, mouseout and click
+
+function removeActiveClass(){
+ chartBars.forEach(el => {
+  let label = el.getElementsByClassName('amount');
+      if (el.classList.contains('active')) {
+        el.classList.remove('active', 'today');
+        label[0].style.visibility = 'hidden';
+      } else {
+        el.classList.remove('active', 'today');
+      }
+    });
+}
+
+chartBars.forEach(bar => {
+  bar.addEventListener('mouseover', () => {
+    let label = bar.getElementsByClassName('amount');
+    let obj = myData.find(o => o.day === bar.id);
+
+    label[0].style.visibility = 'visible';
+    label[0].innerHTML = `$${obj.amount}`;
+  });
+
+  bar.addEventListener('mouseout', () => {
+    if (!bar.classList.contains('active')) {
+      let label = bar.getElementsByClassName('amount');
+      label[0].style.visibility = 'hidden';
+    }
+  });
+ 
+  bar.addEventListener('click', () => {
+    let label = bar.getElementsByClassName('amount');
+
+    if (bar.classList.contains('active')) {
+      removeActiveClass();
+      label[0].style.visibility = 'hidden';
+    } else {
+      removeActiveClass();
+      bar.classList.add('active');
+      label[0].style.visibility = 'visible';
+    }
+  });
+});
+
+setActiveDay();
 summary();
-
-
-
-
-// utworzenie słupków na podstawie danych 
-
-// funkcja na hovery na poszczególne bary i wyświetlenie danych nad nimi
-
-// funkcja na kliknięcie słupka (zmiana koloru i wyświetlenie danych)
+setHeightOfBars();
